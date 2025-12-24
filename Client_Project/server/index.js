d94c768f595f6b1852e2179248fd9f33e78f72b0
arielfainshtein <arielfainshtein@icloud.com>
2025-12-24 09:26:43 +0200

    fixed a bug for loading env config in the correct path

--- Client_Project/server/index.js
+++ Client_Project/server/index.js
@@ -1,5 +1,5 @@
 // imports
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/api');
