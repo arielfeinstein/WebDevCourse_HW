9296d8f474b120de032bb5f65e43f301ae461c74
arielfainshtein <arielfainshtein@icloud.com>
2026-01-28 13:05:25 +0200

    creating data directory if doesn't exist in the database config file

--- Course_Project/config/database.js
+++ Course_Project/config/database.js
@@ -1,7 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
 const DB_PATH = path.join(__dirname, '../data/database.sqlite');
+
+// Ensure the data directory exists
+const dataDir = path.dirname(DB_PATH);
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+    console.log('Created data directory:', dataDir);
+}
 
 // Create a new database connection
 const db = new sqlite3.Database(DB_PATH, (err) => {
