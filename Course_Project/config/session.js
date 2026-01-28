3eac0e189715505a1211417952274b95b4882196
arielfainshtein <arielfainshtein@icloud.com>
2026-01-28 09:22:59 +0200

    refactor: integrate SQLite session store and update dependencies

--- Course_Project/config/session.js
+++ Course_Project/config/session.js
@@ -1,7 +1,15 @@
 const session = require("express-session");
+const SQLiteStore = require('connect-sqlite3')(session);
 const config = require('./envConfig');
+const path = require('path');
 
 module.exports = session({
+  store: new SQLiteStore({
+    db: 'sessions.sqlite',
+    dir: path.join(__dirname, '../data'),
+    table: 'sessions',
+    concurrentDB: true
+  }),
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: false,
